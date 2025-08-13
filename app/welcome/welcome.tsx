808cef44f935b8709495a38e4aba425f506c7fe1
Brandon Douglas <brandondouglas@Brandons-MacBook-Air.local>
2025-08-13 21:33:22 +0100

    added upload button

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,27 +4,32 @@ import logoLight from "./logo-light.svg";
 export function Welcome() {
   return (
     <main>
-    <p className="text-2xl text-center">BudgetWithBrandon</p>
-    <p className="text-center">Welcome! I'm Your AI Personal Finance Coach!</p>
-    <hr></hr>
-    <p className='text-center'>To begin, choose what format you want your transactions to be displayed in.</p>
-    <p className='text-center'>We accept the following <strong>formats</strong>: </p>
-    <ul className="text-center">
-      <li>Adobe Acrobat (PDF)</li>
-      <li>Midata (CSV) </li>
-      <li>Text File (TXT)</li>
-      <li>Microsoft Excel (XLS)</li>
+      <p className="text-2xl text-center">BudgetWithBrandon</p>
+      <p className="text-center">Welcome! I'm Your AI Personal Finance Coach!</p>
+      <hr></hr>
+      <p className='text-center'>To begin, choose what format you want your transactions to be displayed in.</p>
+      <p className='text-center'>We accept the following <strong>formats</strong>: </p>
+      <ul className="text-center">
+        <li>Midata (CSV)✅*</li>
+        <li>Adobe Acrobat (PDF - feature coming soon) ❌</li>
 
-    </ul>
-    <body>
-      <p>hi</p>
-    </body>
-    <hr></hr>
-    <footer><p className='text-center'>Coded by @brandonodouglas</p></footer>
+        <li>Text File (TXT - feature coming soon) ❌</li>
+        <li>Microsoft Excel (XLS - feature coming soon) ❌</li>
+      </ul>
+      <body>
+        <p className="text-center">*Note: For the Santander bank, the midata only goes back a year, but you can choose what duration the app will, months, weeks or days.</p>
+      </body>
+      <hr></hr>
+      <div className="flex justify-center">
+        <button className="border-2 p-2 border-solid mt-5 mb-5 bg-green-500 hover:bg-green-700">⬆️ Upload CSV</button>
+
+      </div>
+
+      <footer><p className='text-center'>Coded by @brandonodouglas</p></footer>
     </main>
-    
-    )
-  }
-  
+
+  )
+}
+
 // Function that displays all the budgets and checks for overlaps 
 
