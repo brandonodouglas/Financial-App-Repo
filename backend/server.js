a54969e8c42ed47c98c7f57ece3c9c9bbfa23078
Brandon Douglas <brandondouglas@Brandons-MacBook-Air.local>
2025-08-20 16:36:20 +0100

    added server file location

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const router = express.Router();
+
+router.get('/', get)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
