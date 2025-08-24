ee98b62e9f3c78fdeeae401d2b6c8197c94ce766
Brandon Douglas <brandondouglas@Brandons-MacBook-Air.local>
2025-08-24 11:11:17 +0100

    added formatting to transaction input box and button

--- frontend/app/components/transactioninput.tsx
+++ frontend/app/components/transactioninput.tsx
@@ -3,8 +3,8 @@ export function TransactionInput() {
   return (
     <div className="flex justify-center items-center bg-blue-400 h-50 w-screen">
       <form>
-        <span className="text-2xl font-extralight">Paste <strong className ="font-extralight">Transactions</strong> Here: </span><input className="bg-amber-50 text-2xl p-1 border-black border-1 text-black m-3" name="query" />
-        <button className="border-black text-2xl border-1 border-white p-1 font-extralight" type="submit">ANALYSE</button>
+      <input placeholder="Paste Transactions Here" className="placeholder-gray-200 bg-amber-50 text-2xl p-3 border-green-300 border-1 text-black m-3" name="query" />
+        <button className="border-green-400 text-2xl border-1 border-red-white p-3 font-extralight" type="submit">ANALYSE</button>
       </form>
 
 
