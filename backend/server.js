14e4655d147739b8f8a0562aeccc76d8cdbe341c
Brandon Douglas <brandondouglas@Brandons-MacBook-Air.local>
2025-08-27 18:26:52 +0100

    Added array database and transaction api ready for database storage

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// Placeholder for mongodb database
+let transactionsDatabase = [];
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// GET Endpoint for transaction data
+app.get('/transactions', (req, res, next) => {
+  res.send('[SERVER]: Here is the transactions database: ' + JSON.stringify(transactionsDatabase));
+
+});
+
+// POST endpoint for transaction data
+app.post('/transactions', (req, res, next) => {
+  const newTransaction = {
+    TransactionData: req.body,
+  };
+  console.log('The request body is: ' + req.body)
+  transactionsDatabase.push(newTransaction.TransactionData.query)
+  console.log(transactionsDatabase[0]);
+  res.send("success");
+});
+
 app.listen(port, () => {
   console.log(`[SERVER]: Example app listening on port ${port}`)
-})
+});
--- frontend/app/components/chatbot.tsx
+++ frontend/app/components/chatbot.tsx
@@ -1,40 +1,43 @@
-
 import { useState } from 'react';
 import React from 'react';
 import { useRef } from 'react';
-
-// This is a message object
-type Message = {
-  text: string,
-  sender: 'ai' | 'user'
-};
+import axios from 'axios';
 
 // https://www.dhiwise.com/post/a-step-by-step-guide-to-retrieving-input-values-in-react
 // Use an uncontrolled component
 
 export function Chatbot() {
+  // sends user input data to the database
   function chatbot(formData: { get: (arg0: string) => any; }) {
     const query = formData.get("query");
     alert(`You searched for '${query}'`);
     // send query to backend
+    const name = "brandon";
     console.log(query);
-  }
+   
 
- 
+    axios.post('http://localhost:3000/transactions', {
+      query,
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
+  };
   return (
     <div className="flex flex-col items-center bg-white h-screen w-screen">
-        <div className="flex flex-col text-black mt-10 text-base/9">
-          <p className="text-black text-5xl font-semibold">Brandroid Chat Bot 🤖💬</p>
-          <p>[BRANDROID]: Hey, I'm Brandroid! 👋😊 . Enter your transaction data below to get started.</p>
-          <form action={chatbot}>
-            <input name="query" className="border-1" type="text" placeholder="Enter transaction data" />
-            <button type="submit">Chatbot</button>
-          </form>
-
-        </div>
-
-
+      <div className="flex flex-col text-black mt-10 text-base/9">
+        <p className="text-black text-5xl font-semibold">Brandroid Chat Bot 🤖💬</p>
+        <p>[BRANDROID]: Hey, I'm Brandroid! 👋😊 . Enter your transaction data below to get started.</p>
+        <form action={chatbot}>
+          <input name="query" className="border-1" type="text" placeholder="Enter transaction data" />
+          <button type="submit">Chatbot</button>
+        </form>
+      </div>
     </div>
-
   );
 }
