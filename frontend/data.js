c21b5a92239e431557d8a11dfe325a909aca4816
Brandon Douglas <brandondouglas@Brandons-MacBook-Air.local>
2025-09-03 20:07:35 +0100

    reset to previous state

---
+++ frontend/data.js
@@ -0,0 +1,6 @@
+const tableData = [
+    { date: new Date("03/09/2025"), description: "Money given to mum", moneyin: 0, moneyout: 50, balance: 50, category: "charity" },
+    { date: new Date("02/09/2025"), description: "rent money to bill", moneyin: 0, moneyout: 160, balance: 100, category: "rent" },
+    { date: new Date("01/09/2025"), description: "pip", moneyin: 160, moneyout: 0, balance: 260, category: "income"},
+
+]
