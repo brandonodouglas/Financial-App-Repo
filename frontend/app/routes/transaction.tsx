3d1725899c6a4948acf83757a93c4d4725f1e49e
Brandon Douglas <brandondouglas@Brandons-MacBook-Air.local>
2025-09-06 16:44:18 +0100

    styled transaction input form into a row of inputs

--- frontend/app/routes/transaction.tsx
+++ frontend/app/routes/transaction.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 
-
+//https://www.w3schools.com/react/showreact.asp?filename=demo_react_forms_multiple
 
 
 
@@ -15,36 +15,38 @@ export default function Transaction() {
     function handleClick() {
         setTimelineCount(timelineCount + 1);
         
-        alert('timeline button clicked');
+        alert('Transaction saved to database');
     }
  
 
   return (
-    <div>
+    <div >
         <h1>Welcome, Brandon.</h1>
-        <p>You currently have { timelineCount } timelimes.</p>
+        <p>You currently have 1 timelimes.</p>
         <hr></hr>
         <br></br>
-        <form>
-        <h2>TIMELINE: { timelineCount }</h2>
-        <p>Date</p>
-        <input className="text-black bg-amber-600 scheme-dark" type="date" />
-        <p>Description</p>
-        <input className="text-black bg-white" type="text" />
-        <p>Money in</p>
-        <input className="text-black bg-white" type="text" />
-        <p>Money out</p>
-        <input className="text-black bg-white" type="text" />
-        <p>Balance</p>
-        <input className="text-black bg-white" type="text" />
-        <p>Category</p>
-        <input className="text-black bg-white" type="text" />
+        <p>Current Transaction To Be Added:</p>
+        <form className="flex row items-center justify-around border-white border-2 border-solid">
         
-      
+        <p>DATE:</p>
+        <input className="text-black bg-amber-600 scheme-dark" type="date" />
+        <p>DESCRIPTION</p>
+        <input className="text-black bg-white" type="text" />
+        <p>MONEY IN</p>
+        <input className="text-black bg-white" type="text" />
+        <p>Money OUT</p>
+        <input className="text-black bg-white" type="text" />
+        <p>BALANCE</p>
+        <input className="text-black bg-white" type="text" />
+        <p>CATEGORY</p>
+        <input className="text-black bg-white" type="text" /> 
+        
+                <button type="submit" onClick={handleClick} className="border-2 border-white border-solid" value="submit">ADD</button>
+
+        
     </form>
     
 
-        <button type="button" onClick={handleClick} className="border-2 border-white border-solid">ADD TIMELINE</button>
     </div>
   )
 
